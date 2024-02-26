51d905b43629bc25c6dc8d67be7745f132c2031d
Luka626 <landj072@uottawa.ca>
2024-02-26 03:10:31 -0500

    forward IK

--- src/uorover_ik/src/ik_hardware_interface.cpp
+++ src/uorover_ik/src/ik_hardware_interface.cpp
@@ -1,6 +1,7 @@
 #include <chrono> // IWYU pragma: keep
 #include <functional> // IWYU pragma: keep
 #include <memory>
+#include <sensor_msgs/msg/detail/joint_state__struct.hpp>
 #include <string>
 
 #include "rclcpp/node.hpp"
@@ -8,8 +9,17 @@
 #include "std_msgs/msg/string.hpp" // IWYU pragma: keep
 #include "general_interfaces/msg/arm_pose.hpp" // IWYU pragma: keep
 #include "control_msgs/msg/joint_trajectory_controller_state.hpp" // IWYU pragma: keep
+#include "sensor_msgs/msg/joint_state.hpp" // IWYU pragma: keep
 
 using namespace std::chrono_literals;
+
+enum Joint{
+    TOWER,
+    SHOULDER,
+    ELBOW,
+    WRIST,
+    LAST
+};
 
 //4096 counts
 struct LinearActuator{
@@ -28,8 +38,10 @@ class IKHardwareInterface : public rclcpp::Node
   public:
     IKHardwareInterface() : Node("HardwareInterface") {
 
+        testing = false;
+
         // replace this with the joint_states publisher type
-        upstream_publisher_ = this->create_publisher<control_msgs::msg::JointTrajectoryControllerState>("joint_states", 10);
+        upstream_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
 
         upstream_subscription_ = this->create_subscription<control_msgs::msg::JointTrajectoryControllerState>(
                 "/uorover_arm_controller/controller_state",
@@ -68,43 +80,78 @@ class IKHardwareInterface : public rclcpp::Node
     }
 
     void setReferencePositions(const std::vector<double> &referencePositions){
-        float tmp;
+        for (int jnt=TOWER; jnt<LAST; jnt++){
+            switch (jnt){
+                case TOWER:
+                    referencePositions_[jnt] = referencePositions[jnt];
+                    break;
+                case SHOULDER:
+                    referencePositions_[jnt] = LA1_.lMin + LA1_.slope * (referencePositions[jnt] - LA1_.thetaMin);
+                    break;
+                case ELBOW:
+                    referencePositions_[jnt] = LA2_.lMax - LA2_.slope * (referencePositions[jnt] - LA2_.thetaMin);
+                    break;
+                case WRIST:
+                    referencePositions_[jnt] = referencePositions[jnt];
+                    break;
+            }
 
-        referencePositions_ = referencePositions;
-        tmp = referencePositions_[1];
-        referencePositions_[1] = LA1_.lMin + LA1_.slope * (tmp - LA1_.thetaMin);
-        tmp = referencePositions_[2];
-        referencePositions_[2] = LA2_.lMax - LA2_.slope * (tmp - LA2_.thetaMin);
-    } 
+        }
+    }
 
     std::vector<double> getArmPositions() const {
         return armPositions_;
     }
 
     void setArmPositions(const std::vector<double> &armPositions){
-        armPositions_ = armPositions;
+        for (int jnt=TOWER; jnt<LAST; jnt++){
+            switch (jnt){
+                case TOWER:
+                    armPositions_[jnt] = armPositions[jnt];
+                    break;
+                case SHOULDER:
+                    armPositions_[jnt] = LA1_.thetaMin + (1/LA1_.slope) * (armPositions[jnt] - LA1_.lMin);
+                    break;
+                case ELBOW:
+                    armPositions_[jnt] = LA2_.thetaMax - (1/LA2_.slope) * (armPositions[jnt] - LA2_.lMin);
+                    break;
+                case WRIST:
+                    armPositions_[jnt] = armPositions[jnt];
+                    break;
+            }
+        }
     } 
 
   private:
     // on controller state topic callback, update reference positions attribute
     void ik_callback(const control_msgs::msg::JointTrajectoryControllerState &msg){
-
         auto message = general_interfaces::msg::ArmPose();
 
         this->setReferencePositions(msg.reference.positions);
-
         message.positions = this->getReferencePositions();
 
         downstream_publisher_->publish(message);
     }
 
     void arm_callback(const general_interfaces::msg::ArmPose &msg){
-        this->setArmPositions(msg.positions);
+        auto message = sensor_msgs::msg::JointState();
+        std::vector<std::string> names = {"q1", "q2", "q3", "q4"};
+        message.name = names;
+
+        if (!testing){
+            this->setArmPositions(msg.positions);
+        } else{
+            armPositions_ = referencePositions_;
+        }
+
+        message.position = this->getArmPositions();
+
+        upstream_publisher_->publish(message);
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
 
-    rclcpp::Publisher<control_msgs::msg::JointTrajectoryControllerState>::SharedPtr upstream_publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr upstream_publisher_;
     rclcpp::Publisher<general_interfaces::msg::ArmPose>::SharedPtr downstream_publisher_;
 
     rclcpp::Subscription<control_msgs::msg::JointTrajectoryControllerState>::SharedPtr upstream_subscription_;
@@ -115,6 +162,8 @@ class IKHardwareInterface : public rclcpp::Node
 
     LinearActuator LA1_;
     LinearActuator LA2_;
+
+    bool testing;
 
 };
 
