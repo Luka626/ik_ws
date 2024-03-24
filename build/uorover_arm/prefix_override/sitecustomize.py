c8c6dbafcceff7b9a769a64f132b25e8b6fbd537
Luka626 <61366851+Luka626@users.noreply.github.com>
2024-03-24 00:40:21 -0400

    Delete build directory

--- build/uorover_arm/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/luka/ws_moveit2/install/uorover_arm'
--- build/uorover_moveit_config/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/luka/ws_moveit2/install/uorover_arm/share/uorover_arm/package.sh"
